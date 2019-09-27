15256ec1e0d4ecec0cddb05e8cfffc8b07f22150
Marcello Ghiozzi <m.ghiozzi@switchup.it>
2019-09-27 14:42:30 +0200

    Firebase Dynamic Link (open with Chrome) now supported

--- src/android/io/ionic/links/IonicDeeplink.java
+++ src/android/io/ionic/links/IonicDeeplink.java
@@ -71,7 +71,7 @@ public class IonicDeeplink extends CordovaPlugin {
     Log.d(TAG, "Got a new intent: " + intentString + " " + intent.getScheme() + " " + action + " " + url);
 
     // if app was not launched by the url - ignore
-    if (!Intent.ACTION_VIEW.equals(action) || url == null) {
+    if (url == null) {
       return;
     }
 
